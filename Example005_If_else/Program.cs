944b85d49f5c7695af47491dea03bcccbc805e82
Ekaterina <katerinaboeva17@gmail.com>
2022-06-09 12:54:42 +0300

    Example005_if_else

---
+++ Example005_If_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "катя")
+{
+     Console.WriteLine("Ура, это же Катя!");
+} 
+else
+{
+     Console.Write("Привет, ");
+     Console.WriteLine(username);
+}
+
